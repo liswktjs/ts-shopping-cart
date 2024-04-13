2c80ab7c41c5759ceb5b515abbf6fe98e8bb19b0
JASUN LEE <liswktjs@gmail.com>
2024-04-13 14:24:19 +0900

    fix: color token에서 blue컬러를 mint 컬러로 네이밍 수정

--- src/styles/token.stylex.ts
+++ src/styles/token.stylex.ts
@@ -2,8 +2,8 @@ import * as stylex from "@stylexjs/stylex";
 
 export const colors = stylex.defineVars(
   {
-    blue_500: "#22A6A2",
-    blue_400: "#2AC1BC",
+    mint_500: "#22A6A2",
+    mint_400: "#2AC1BC",
     white: "#ffffff",
     black: "#333333",
     brown: "#73675C",

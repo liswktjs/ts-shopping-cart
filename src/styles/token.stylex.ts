f60e0dac3759bab0b4ce1e4ca15b29e740d4a4d9
JASUN LEE <liswktjs@gmail.com>
2024-04-13 13:29:05 +0900

    feat:  project에서 사용되는 color token들 정의

---
+++ src/styles/token.stylex.ts
@@ -0,0 +1,14 @@
+import * as stylex from "@stylexjs/stylex";
+
+export const colors = stylex.defineVars(
+  {
+    blue_500: "#22A6A2",
+    blue_400: "#2AC1BC",
+    white: "#ffffff",
+    black: "#333333",
+    brown: "#73675C",
+    zinc_500: "#AAAAAA",
+    zinc_400: "#DDDDDD",
+    zinc_300: "#F6F6F6",
+  }
+);
